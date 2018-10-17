2b42544cee1e17cf3657608fb66c1a9f42d3dc7c
Gargaj <gargaj@conspiracy.hu>
2018-10-17 16:24:51 +0200

    fix longstanding bug where differing number of entries caused entry storage to get messed up

--- www_admin/plugins/draggable_compoentries/ajax_reorder.php
+++ www_admin/plugins/draggable_compoentries/ajax_reorder.php
@@ -11,15 +11,23 @@ function _rename($a,$b)
   return rename($a,$b);
 }
 
+$entries = SQLLib::selectRows(sprintf_esc("select * from compoentries where compoid = %d",$_POST["compo"]));
+
+if (count($entries) != count($_POST["order"]))
+{
+  die("WARNING: the number of releases changed in this compo while you were reordering them; please reload the page and try again!");
+}
+
 // step1: move all entries to temporary folder named by entrynumber
 
-$compo = get_compo($_POST["compo"]);
-_rename($settings["private_ftp_dir"] . "/" . $compo->dirname, $settings["private_ftp_dir"] . "/" . $compo->dirname . ".$tmp$");  
+$SUFFIX = ".\$tmp\$/";
 
-$entries = SQLLib::selectRows(sprintf_esc("select * from compoentries where compoid = %d",$_POST["compo"]));
+$compo = get_compo($_POST["compo"]);
+_rename($settings["private_ftp_dir"] . "/" . $compo->dirname, $settings["private_ftp_dir"] . "/" . $compo->dirname . $SUFFIX);  
+
 foreach($entries as $entry)
 {
-  $root = $settings["private_ftp_dir"] . "/" . $compo->dirname . ".$tmp$/";
+  $root = $settings["private_ftp_dir"] . "/" . $compo->dirname . $SUFFIX;
   _rename($root.sprintf("%03d",$entry->playingorder),$root.sprintf("_%03d",$entry->id));
 }
 
@@ -31,14 +39,14 @@ foreach($entries as $entry)
 $n = 1;
 foreach($_POST["order"] as $v)
 {
-  $oldroot = $settings["private_ftp_dir"] . "/" . $compo->dirname . ".$tmp$/";
+  $oldroot = $settings["private_ftp_dir"] . "/" . $compo->dirname . $SUFFIX;
   $newroot = get_compo_dir( $compo );
   _rename($oldroot.sprintf("_%03d",$v),$newroot.sprintf("%03d",$n));
   SQLLib::Query(sprintf_esc("update compoentries set playingorder = %d where id = %d",$n,$v));
   $n++;
 }
 
-if (!rmdir( $settings["private_ftp_dir"] . "/" . $compo->dirname . ".$tmp$/" ))
+if (!rmdir( $settings["private_ftp_dir"] . "/" . $compo->dirname . $SUFFIX ))
   die("ERROR deleting");
 
 ?>SUCCESS
