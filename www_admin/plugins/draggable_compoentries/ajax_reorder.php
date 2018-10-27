5fac937ddde19683fc11b5afd9e962f83f5de0f2
Gargaj <gargaj@conspiracy.hu>
2018-10-27 19:56:25 +0200

    fix up a proper solution to behind-the-scenes data change issue (fixes #32)

--- www_admin/plugins/draggable_compoentries/ajax_reorder.php
+++ www_admin/plugins/draggable_compoentries/ajax_reorder.php
@@ -13,11 +13,6 @@ function _rename($a,$b)
 
 $entries = SQLLib::selectRows(sprintf_esc("select * from compoentries where compoid = %d",$_POST["compo"]));
 
-if (count($entries) != count($_POST["order"]))
-{
-  die("WARNING: the number of releases changed in this compo while you were reordering them; please reload the page and try again!");
-}
-
 // step1: move all entries to temporary folder named by entrynumber
 
 $SUFFIX = ".\$tmp\$/";
@@ -25,10 +20,17 @@ $SUFFIX = ".\$tmp\$/";
 $compo = get_compo($_POST["compo"]);
 _rename($settings["private_ftp_dir"] . "/" . $compo->dirname, $settings["private_ftp_dir"] . "/" . $compo->dirname . $SUFFIX);  
 
+$list = array();
 foreach($entries as $entry)
 {
   $root = $settings["private_ftp_dir"] . "/" . $compo->dirname . $SUFFIX;
-  _rename($root.sprintf("%03d",$entry->playingorder),$root.sprintf("_%03d",$entry->id));
+  $old = $root.sprintf("%03d",$entry->playingorder);
+  $new = $root.sprintf("_%03d",$entry->id);
+  if (file_exists($old))
+  {
+    _rename($old,$new);
+    $list[$entry->id] = true;
+  }
 }
 
 // step2: move entries back by correct order 
@@ -36,14 +38,35 @@ foreach($entries as $entry)
 @mkdir( get_compo_dir( $compo ) );
 @chmod( get_compo_dir( $compo ), 0777 );
 
+$newOrder = array();
+
+// take the desired order, add any "new" entries to the end
+foreach($_POST["order"] as $entryID)
+{
+  $newOrder[] = $entryID;
+  unset($list[$entryID]);
+}
+foreach($list as $entryID=>$true)
+{
+  $newOrder[] = $entryID;
+}
+
 $n = 1;
-foreach($_POST["order"] as $v)
+foreach($newOrder as $entryID)
 {
   $oldroot = $settings["private_ftp_dir"] . "/" . $compo->dirname . $SUFFIX;
   $newroot = get_compo_dir( $compo );
-  _rename($oldroot.sprintf("_%03d",$v),$newroot.sprintf("%03d",$n));
-  SQLLib::Query(sprintf_esc("update compoentries set playingorder = %d where id = %d",$n,$v));
-  $n++;
+  $olddir = $oldroot.sprintf("_%03d",$entryID);
+  $newdir = $newroot.sprintf("%03d",$n);
+  
+  // if it was deleted since, just skip it
+  if (file_exists($olddir))
+  {
+    _rename($olddir,$newdir);
+    SQLLib::Query(sprintf_esc("update compoentries set playingorder = %d where id = %d",$n,$entryID));
+    $n++;
+    unset($list[$entryID]);
+  }
 }
 
 if (!rmdir( $settings["private_ftp_dir"] . "/" . $compo->dirname . $SUFFIX ))
